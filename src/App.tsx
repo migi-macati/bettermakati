e6b59324f2f92e49c11cfb11dcd948c6a751fdc1
migi-macati <valeroso.miguel@gmail.com>
2026-09-19 19:12:24 +0800

    Complete open-government doctrine integration and monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,7 @@ import PublicRecords from './pages/PublicRecords';
 import Today from './pages/Today';
 import OpenGovernment from './pages/OpenGovernment';
 import Integrity from './pages/Integrity';
+import ProjectStatus from './pages/ProjectStatus';
 
 function App() {
   return (
@@ -82,6 +83,7 @@ function App() {
                 <Route path="/today" element={<Today />} />
                 <Route path="/open-government" element={<OpenGovernment />} />
                 <Route path="/integrity" element={<Integrity />} />
+                <Route path="/status" element={<ProjectStatus />} />
                 <Route path="/barangays" element={<Barangays />} />
                 <Route path="/barangays/:slug" element={<BarangayProfile />} />
                 <Route path="/elections" element={<Elections />} />
