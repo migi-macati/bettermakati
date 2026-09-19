adea1a033a25736ebda95655d851a3601ea6cdc6
migi-macati <valeroso.miguel@gmail.com>
2026-09-19 12:14:47 +0800

    Strengthen institutional trust, contact UX and public-facing consistency

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -333,7 +333,7 @@ const Home: React.FC = () => {
             ))}
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 mt-8">
             {cityPaths.map(item => {
               const Icon = item.icon;
               return (
