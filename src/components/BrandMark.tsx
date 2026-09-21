cb70b40a91597717201c06b0b4926195d45437dd
migi-macati <valeroso.miguel@gmail.com>
2026-09-21 17:42:40 +0800

    Use exact official BetterMakati logo lockup

--- src/components/BrandMark.tsx
+++ src/components/BrandMark.tsx
@@ -10,40 +10,20 @@ export default function BrandMark({
   return (
     <Link
       to="/"
-      className="flex items-center gap-3 group"
+      className="inline-flex items-center"
       aria-label="BetterMakati home"
     >
       <img
-        src={inverse ? '/bettermakati-mark-white.svg' : '/bettermakati-mark.svg'}
-        width={compact ? 48 : 56}
-        height={compact ? 36 : 44}
-        alt=""
-        aria-hidden="true"
-        className={compact ? 'h-9 w-12 shrink-0' : 'h-11 w-14 shrink-0'}
+        src={inverse ? '/bettermakati-logo-reverse.svg' : '/bettermakati-logo.svg'}
+        width={compact ? 116 : 132}
+        height={compact ? 72 : 82}
+        alt="BetterMakati"
+        className={
+          compact
+            ? 'h-[56px] w-auto shrink-0 object-contain'
+            : 'h-[62px] w-auto shrink-0 object-contain sm:h-[68px]'
+        }
       />
-      <div className="leading-tight min-w-0">
-        <div
-          className={
-            compact
-              ? 'brand-wordmark text-lg'
-              : 'brand-wordmark text-xl'
-          }
-        >
-          <span className={inverse ? 'text-white' : 'brand-wordmark-better'}>Better</span>
-          <span className={inverse ? 'text-white' : 'brand-wordmark-makati'}>Makati</span>
-        </div>
-        {!compact && (
-          <div
-            className={
-              inverse
-                ? 'mt-0.5 hidden min-[400px]:block text-xs text-white/72'
-                : 'mt-0.5 hidden min-[400px]:block text-xs text-gray-600'
-            }
-          >
-            Independent civic platform for Makati
-          </div>
-        )}
-      </div>
     </Link>
   );
 }
