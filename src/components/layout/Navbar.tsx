78217ff6bfd40e9eb4d281773bc191ad29a316cb
migi-macati <valeroso.miguel@gmail.com>
2026-09-23 20:19:10 +0800

    Render BetterBarangay context under main header

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -10,6 +10,7 @@ import {
 import { mainNavigation } from '../../data/navigation';
 import { Link, useLocation } from 'react-router';
 import BrandMark from '../BrandMark';
+import BetterBarangayContextBar from '../barangay/BetterBarangayContextBar';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -196,6 +197,7 @@ export default function Navbar() {
             </div>
           </div>
         </div>
+        <BetterBarangayContextBar />
         <div
           id="mobile-navigation"
           hidden={!isOpen}
